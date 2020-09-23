10b1d1085ea3d0623dd42d0febb5730aa69cd8b4
martenkalkman <mkalkman@roc-dev.com>
2020-09-23 12:27:10 +0200

    First java

---
+++ src/h01/Vlag.java
@@ -0,0 +1,19 @@
+import java.awt.*;
+import java.applet.*;
+
+public class Vlag extends Applet {
+
+    public void init() {
+    }
+
+    public void paint(Graphics g) {
+        g.setColor(Color.red);
+        g.fillRect(100, 50, 250, 50);
+        g.setColor(Color.white);
+        g.fillRect(100, 100, 250, 50);
+        g.setColor(Color.blue);
+        g.fillRect(100, 150, 250, 50);
+        g.setColor(Color.black);
+        g.fillRect(100, 50, 10, 500);
+    }
+}
